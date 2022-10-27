90876f030ce36ffdb92f8d33e84126251d48fce9
jonahmarc <olympusfoxtrot@gmail.com>
2022-10-27 19:09:19 +0800

    added initial homepage, and changed pages folder structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
-import Login from './pages/login.component';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+import Login from "./pages/login/login.component";
+import Home from "./pages/home/home.component";
+
 
 function App() {
   return (
     <div>
-      <Login />
+      {/* <Login /> */}
+      <Home />
     </div>
   );
 }
